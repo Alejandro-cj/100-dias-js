dd3d55a284172b6a25a57e9d8e694f97b8d1ab83
Alejandro-cj <aloflow_345@hotmail.com>
2024-01-10 21:35:33 -0500

    dia 1 completado

---
+++ proyectos/parte-1/dia1/day-1.js
@@ -0,0 +1,15 @@
+// Declaración de una variable llamada 'nombre' y asignación de un valor.
+let nombre = "Juan";
+
+// Estructura condicional (if-else) para verificar si 'nombre' es igual a "Juan".
+if (nombre === "Juan") {
+  console.log("¡Hola, Juan!"); // Imprime un saludo si la condición es verdadera.
+} else {
+  console.log("Hola, desconocido."); // Imprime otro mensaje si la condición es falsa.
+}
+
+// Bucle (for) para imprimir números del 0 al 4.
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+  // La variable 'i' se incrementa en cada iteración.
+}
---
+++ proyectos/parte-1/dia1/prueba.js
@@ -0,0 +1,11 @@
+// Crea un programa simple que solicite el nombre del usuario y le dé la bienvenida en la consola.
+
+let nombre = "Alejandro";
+
+if (nombre === "Alejandro") { // si cambia mi nombre cambiara el mensaje xd novato
+    console.log ("Bienvenido, Alejandro!");
+} else {
+    console.log("Bienvenido, desconocido!");
+}
+
+//
